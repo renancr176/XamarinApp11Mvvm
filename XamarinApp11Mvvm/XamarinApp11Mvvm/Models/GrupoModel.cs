593f0e45f34db61ae0e407a74007c3a5e09efd46
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2019-02-08 09:27:57 -0200

    Estrutura MVVM criada

---
+++ XamarinApp11Mvvm/XamarinApp11Mvvm/Models/GrupoModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp11Mvvm.Models
+{
+    class GrupoModel
+    {
+    }
+}
---
+++ XamarinApp11Mvvm/XamarinApp11Mvvm/ViewsModels/JogoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp11Mvvm.ViewsModels
+{
+    class JogoViewModel
+    {
+    }
+}
